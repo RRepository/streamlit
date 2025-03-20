227072c1075b3b12e05fd39d25661d3f1b7939e4
Rakesh ch <rakeshghub@gmail.com>
2025-03-20 11:00:11 +0530

    commit

--- Home.py
+++ Home.py
@@ -22,13 +22,13 @@ st.divider()
 col1, col2, col3 = st.columns(3)
 
 with col1:
-    st.link_button("Go to Sales Dashboard ->", "http://localhost:8501/Sales_Dashboard")
+    st.link_button("Go to Sales Dashboard ->", "https://app-4uhyvwebwby8t9kcvylwzz.streamlit.app/Sales_Dashboard")
     
 with col2:
-    st.link_button("Generate Invoice ->", "http://localhost:8501/Invoice_generator")
+    st.link_button("Generate Invoice ->", "https://app-4uhyvwebwby8t9kcvylwzz.streamlit.app/Invoice_generator")
     
 with col3:
-    st.link_button("Deep Dive ->", "http://localhost:8501/Deep_dive_into_data")
+    st.link_button("Deep Dive ->", "https://app-4uhyvwebwby8t9kcvylwzz.streamlit.app/Deep_dive_into_data")
 
 
 
